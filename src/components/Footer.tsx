d76ca6f1a6aa419b88b9db38e14e86e6db8ea72c
tditada <tditada@gmail.com>
2022-08-08 14:04:19 +0200

    styling footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const FOOTER_TEXT = 'Hecho por @masalladelpapel (instagram/tiktok)';
+const FOOTER_TEXT = "Hecho por ";
+const LINK_ACCOUNT = "@masalladelpapel";
 
 const StyledFooter = styled.footer`
-    margin-top: 3em;
-    text-align: center;
+  margin-top: 3em;
+  text-align: center;
+`;
+
+const StyledLink = styled.a`
+  display: inline-block;
+  color: white;
 `;
 
 const Footer = () => {
-    return (
-        <StyledFooter>_
-            <p>{FOOTER_TEXT}</p>
-        </StyledFooter>
-    );
+  return (
+    <StyledFooter>
+      <p>
+        {FOOTER_TEXT}
+        <StyledLink href="https://www.instagram.com/masalladelpapel/">
+          {LINK_ACCOUNT}
+        </StyledLink>
+      </p>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
